7e6a92e1665631c7ee2c662615f7dce5b2b0f214
Lalit Sharma <38866385+LalitSharma7@users.noreply.github.com>
2021-10-10 10:52:09 +0530

    Update Word Frequency.py

--- Word Frequency.py
+++ Word Frequency.py
@@ -1,1 +1,17 @@
+def WordCount(s):
+    count = dict()
+    words = s.split()
+    
+    for word in words:
+        if word in count:
+            count[word]+=1
+        else:
+            count[word]=1
+    return count
+ 
 
+s = input()
+hel = {}
+hel = WordCount(s)
+
+print(hel)
