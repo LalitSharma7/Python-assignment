6be0e27e961a1bc78f7fab1266fb35b1d4808e18
Lalit Sharma <38866385+LalitSharma7@users.noreply.github.com>
2021-10-10 13:09:57 +0530

    Create Conversion.py

---
+++ Conversion.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+order = input()
+order1 = order.split()
+
+m = int(order1[0])
+n = int(order1[1])
+
+list1 = input()
+input_list = list1.split()
+
+if m*n == len(input_list):
+    # Write your logic here
+    arr1 = np.array(input_list)
+    arr2 = arr1.reshape(m,n)
+    df1 = pd.Dataframe(arr2)
+    print(df1)
+else:
+    print("Weird Order!")
